df8d92e3af83204e90ebd5565cd630fb726832ae
Maheswara Reddy <mahisince2002@gmail.com>
2026-04-13 10:41:33 +0530

    fix(routes): correct enforceIsolation middleware import

--- server/src/routes/complaints.routes.js
+++ server/src/routes/complaints.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { protect, authorize } = require('../middlewares/auth.middleware');
 const validate = require('../middlewares/validate.middleware');
-const enforceIsolation = require('../middlewares/isolation.middleware');
+const { enforceIsolation } = require('../middlewares/isolation.middleware');
 const {
     createComplaint,
     updateComplaintStatus,
